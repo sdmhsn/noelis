fd0899d32a95c5bad38ee8070b54db5ad60071fd
Saddam Husein <saddambinhusein2@gmail.com>
2022-02-17 12:16:52 +0800

    Edit 1 - articles/urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.articles, name='articles'),
+    path('category/<str:cats>/', views.categoryView, name='category'),
+    path('article/', views.article, name='article'),
 ]
