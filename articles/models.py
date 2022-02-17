2e29224ddd17ffb4b3dbed1c627a417493944325
Saddam Husein <saddambinhusein2@gmail.com>
2022-02-17 12:09:15 +0800

    Edit 1 - articles/models.py

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from tinymce.models import HTMLField
+import uuid
+
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    body = HTMLField()
+    thumbnail = models.ImageField(null=True, blank=True, default='default.jpg')
+    categories = models.ManyToManyField('Category', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
