67f5749034f2bd4178e3d61978cd3f7de608d205
Saddam Husein <saddambinhusein2@gmail.com>
2022-02-18 09:09:50 +0800

    Edit 2 - articles/urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.articles, name='articles'),
-    path('category/<str:cats>/', views.categoryView, name='category'),
-    path('article/', views.article, name='article'),
+    path('<str:cats>/', views.categoryView, name='category'),
+    path('article/<str:slugInput>/', views.article, name='article'),
 ]
